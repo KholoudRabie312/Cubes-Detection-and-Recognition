67c393b237761eb2ad0f39e6a2c40ff5f1530782
lenovo <lenovo>
2016-11-17 14:26:13 +0200

    segmentation function has been added

--- phase1/ShapesSegmentation.m
+++ phase1/ShapesSegmentation.m
@@ -3,4 +3,11 @@ function segmentedImage = ShapesSegmentation(enhancedImage)
 %apply segmentation steps (Edge segmentation) on the enhancedImage and return segmentedImage
 %READABLE and COMMENTED please!
 
+img=enhancedImage;
+img = rgb2gray(img); 
+segmentedImage= edge(img, 'canny', .3);
+ imshow(segmentedImage);  
+
+
+
 end
